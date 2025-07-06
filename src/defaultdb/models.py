2cb10497016db168d935e6c58dbd749fab430e09
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-07-06 13:24:00 +0900

    🗃️

--- src/defaultdb/models.py
+++ src/defaultdb/models.py
@@ -2,6 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Group(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=256, unique=True)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='group_name_uniq'),
+        ]
+        db_table = "group"
+
+
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,6 +22,15 @@ class User(models.Model):
 
     class Meta:
         db_table = "user"
+
+
+class GroupUserRelation(models.Model):
+    id = models.AutoField(primary_key=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "group_user_relation"
 
 
 class Tag(models.Model):
